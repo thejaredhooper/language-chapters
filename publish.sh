a4e63e0b89751a7a2e07b5ee8ce695cd6c407985
Jared Hooper <j.red.hooper@gmail.com>
2019-04-02 16:10:49 -0400

    Published via script ->

--- publish.sh
+++ publish.sh
@@ -2,7 +2,7 @@ VERSION=$(npm version patch)
 VERSION=$(echo $VERSION | cut -c 1-)
 
 git add .
-git commit -m"Published via script -> `$VERSION`"
+git commit -m"Published via script -> $(VERSION)"
 git push
 
 npm publish
