cfead0a549c09cb48cc4d7aa9958801f53443393
Jared Hooper <j.red.hooper@gmail.com>
2019-04-02 16:05:46 -0400

    Published via script

--- publish.sh
+++ publish.sh
@@ -1,6 +1,7 @@
 PACKAGE_VERSION=$(npm version patch)
 
-npm version patch \
-  && git push \
-  && npm publish \
-  && apm publish --tag $PACKAGE_VERSION
+git add .
+git commit -m"Published via script"
+git push
+npm publish
+apm publish --tag $PACKAGE_VERSION
