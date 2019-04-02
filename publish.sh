cb3c8980e780c4011a59ba089f6f9b130e338c91
Jared Hooper <j.red.hooper@gmail.com>
2019-04-02 16:12:04 -0400

    Automated Commit

--- publish.sh
+++ publish.sh
@@ -2,7 +2,7 @@ VERSION=$(npm version patch)
 VERSION=$(echo $VERSION | cut -c 1-)
 
 git add .
-git commit -m"Published Version Through Script: $(echo $VERSION)"
+git commit -m"Automated Commit"
 git push
 
 npm publish
